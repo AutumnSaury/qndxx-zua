a00a134f2041b972faa2e781cc81938a686ada8e
秋サンマ <autumnsaury@outlook.com>
2023-05-31 14:00:08 +0800

    ✨feature: remove the function of exporting screenshot of zhqn login screen

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,65 @@
+<template>
+  <button id="scroll-btn" ref="scrollBtn" @click.prevent="handleScroll">
+    ↑
+  </button>
+  <div id="preview-container" ref="previewContainer">
+    <canvas v-show="drawn" id="preview" ref="preview" width="1080" height="2160" />
+  </div>
+  <fieldset id="control" ref="control">
+    <div id="title">
+      设置
+    </div>
+    <fieldset class="section">
+      <legend>个人信息</legend>
+      <div class="pair">
+        <label for="name">姓名</label>
+        <input id="name" v-model="name" type="text" placeholder="请输入姓名">
+      </div>
+      <div class="pair">
+        <label for="id">学号</label>
+        <input id="id" v-model.number="id" type="text" placeholder="请输入学号">
+      </div>
+      <div id="info-action">
+        <button @click.prevent="submitInfo(name, id)">
+          提交信息
+        </button>
+        <button @click.prevent="reDraw">
+          刷新预览窗口
+        </button>
+      </div>
+    </fieldset>
+    <fieldset class="section">
+      <legend>导出设置</legend>
+      <div class="pair">
+        <label for="filename2">青年大学习截图文件名</label>
+        <input id="filename2" v-model="fileName" type="text" :placeholder="`${id}${name}.jpg`">
+      </div>
+      <button @click.prevent="download">
+        下载文件
+      </button>
+    </fieldset>
+  </fieldset>
+  <div id="top-ellipse" />
+  <div id="bottom-ellipse" />
+</template>
+
 <script setup>
-import html2canvas from 'html2canvas'
-import { ref, watch, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import statusBarImg from './assets/statusBar.png'
 import api from './api/api.js'
 import './assets/check-mark.svg'
 import './assets/empty.svg'
 
-// 郑航青年网页加载完成
-const zhqnLoaded = ref(false)
 // 预览canvas
 const preview = ref(null)
-// 郑航青年iframe
-const frame = ref(null)
 // 姓名
 const name = ref('')
 // 学号
 const id = ref('')
-// 选择的预览页面
-const zhqnSelected = ref(null)
-// 郑航青年页面截图文件名
-const fileName1 = ref('')
 // 青年大学习结束截图文件名
-const fileName2 = ref('')
+const fileName = ref('')
 // 预览图已绘制？
 const drawn = ref(false)
-// 主窗体canvas
-let zhqn
-
-async function renderZhqn () {
-  if (!zhqnLoaded.value) {
-    alert('未加载完成')
-    zhqnSelected.value = null
-    return
-  }
-  const phArr = Array.from(frame.value.contentDocument.getElementsByClassName('input-placeholder'))
-  // Make place-holders look like real input
-  phArr.forEach(item => { item.style.color = 'black' })
-  // Fill place-holders with given information
-  phArr[0].textContent = name.value
-  phArr[1].textContent = id.value.toString()
-  // Render the page into canvas
-  zhqn = await html2canvas(frame.value.contentDocument.getElementsByTagName('uni-app')[0], { scale: 2.5 })
-  await drawPreview(zhqn, getAffixedTime(), '郑航青年大学习')
-}
 
 async function renderCyol () {
   const lessonID = await api.getLatestLessonID()
@@ -55,10 +72,8 @@ async function renderCyol () {
 
 async function download () {
   await submitInfo(name.value, id.value)
-  await renderZhqn()
-  saveAs(preview.value.toDataURL('image/jpeg'), fileName1.value || `${id.value}${name.value}(1).jpg`)
   await renderCyol()
-  saveAs(preview.value.toDataURL('image/jpeg'), fileName2.value || `${id.value}${name.value}(2).jpg`)
+  saveAs(preview.value.toDataURL('image/jpeg'), fileName.value || `${id.value}${name.value}.jpg`)
 }
 
 // Prefix 12 hour time
@@ -103,19 +118,12 @@ async function renderStatusBar (height, width, templateUrl, title, time) {
   return bar
 }
 
-function initFrame () {
-  const bg = frame.value.contentDocument.getElementsByClassName('bg')[0]
-  // Change bg's style
-  bg.className = ''
-  bg.style.background = 'linear-gradient(261deg, hsla(0, 0%, 63.9%, .09), hsla(0, 0%, 63.9%, .09) 33.3%, hsla(0, 0%, 39.2%, .09) 0, hsla(0, 0%, 39.2%, .09) 66.6%, hsla(0, 0%, 63.5%, .09) 0, hsla(0, 0%, 63.5%, .09) 99%),linear-gradient(107deg, hsla(0, 0%, 75.7%, .06), hsla(0, 0%, 75.7%, .06) 33.3%, hsla(0, 0%, 66.3%, .06) 0, hsla(0, 0%, 66.3%, .06) 66.6%, rgba(92, 92, 92, .06) 0, rgba(92, 92, 92, .06) 99%),linear-gradient(338deg, rgba(45, 45, 45, .03), rgba(45, 45, 45, .03) 33.3%, hsla(0, 0%, 87.5%, .03) 0, hsla(0, 0%, 87.5%, .03) 66.6%, hsla(0, 0%, 67.8%, .03) 0, hsla(0, 0%, 67.8%, .03) 99%),linear-gradient(129deg, hsla(0, 0%, 88.6%, .06), hsla(0, 0%, 88.6%, .06) 33.3%, rgba(81, 81, 81, .06) 0, rgba(81, 81, 81, .06) 66.6%, hsla(0, 0%, 72.9%, .06) 0, hsla(0, 0%, 72.9%, .06) 99%),linear-gradient(294deg, hsla(0, 0%, 88.2%, .04), hsla(0, 0%, 88.2%, .04) 33.3%, rgba(95, 95, 95, .04) 0, rgba(95, 95, 95, .04) 66.6%, rgba(39, 39, 39, .04) 0, rgba(39, 39, 39, .04) 99%),linear-gradient(337deg, hsla(0, 0%, 72.2%, .06), hsla(0, 0%, 72.2%, .06) 33.3%, rgba(0, 0, 0, .06) 0, rgba(0, 0, 0, .06) 66.6%, hsla(0, 0%, 54.9%, .06) 0, hsla(0, 0%, 54.9%, .06) 99.9%),linear-gradient(172deg, rgba(40, 40, 40, .07), rgba(40, 40, 40, .07) 33.3%, hsla(0, 0%, 83.9%, .07) 0, hsla(0, 0%, 83.9%, .07) 66.6%, hsla(0, 0%, 74.5%, .07) 0, hsla(0, 0%, 74.5%, .07) 99.9%),linear-gradient(270deg, hsla(0, 0%, 90.2%, 0), hsla(0, 0%, 90.2%, 0) 33.3%, hsla(0, 0%, 94.5%, 0) 0, hsla(0, 0%, 94.5%, 0) 66.6%, rgba(55, 55, 55, 0) 0, rgba(55, 55, 55, 0) 99%),linear-gradient(209deg, #2672e3, #0befc9)'
-  bg.style.height = '100vh'
-}
-
 function loadImage (url) {
-  return new Promise(resolve => {
+  return new Promise((resolve, reject) => {
     const image = new Image()
     image.src = url
     image.onload = () => resolve(image)
+    image.onerror = reject
   })
 }
 
@@ -136,12 +144,8 @@ function saveAs (url, fileName) {
   dllink.remove()
 }
 
-async function reDraw (nv = zhqnSelected.value) {
-  if (nv) {
-    await renderZhqn()
-  } else if (nv === false) {
-    await renderCyol()
-  }
+async function reDraw () {
+  await renderCyol()
 }
 
 async function submitInfo (name, id) {
@@ -177,8 +181,6 @@ function handleScrollBtnTurnOver (deg) {
   scrollBtn.value.style.rotate = deg
 }
 
-watch(zhqnSelected, reDraw)
-
 onMounted(async () => {
   notInViewport = control.value
   onLeave(previewContainer.value, () => {
@@ -189,88 +191,10 @@ onMounted(async () => {
     handleScrollBtnTurnOver('0deg')
     notInViewport = previewContainer.value
   })
+
+  await reDraw()
 })
 </script>
-
-<template>
-  <iframe
-    id="frame"
-    ref="frame"
-    src="/zhqn"
-    frameborder="0"
-    @load="
-      initFrame();
-      zhqnLoaded = true;
-    "
-  />
-  <button id="scroll-btn" ref="scrollBtn" @click.prevent="handleScroll">
-    ↑
-  </button>
-  <div id="preview-container" ref="previewContainer">
-    <canvas v-show="drawn" id="preview" ref="preview" width="1080" height="2160" />
-  </div>
-  <fieldset id="control" ref="control">
-    <div id="title">
-      设置
-    </div>
-    <fieldset id="switch" class="section">
-      <legend>预览切换</legend>
-      <input
-        id="zhqn"
-        v-model="zhqnSelected"
-        class="radio"
-        :value="true"
-        type="radio"
-        name="prev"
-      >
-      <label for="zhqn" :class="{ selected: zhqnSelected }">郑航青年</label>
-      <input
-        id="cyol"
-        v-model="zhqnSelected"
-        class="radio"
-        :value="false"
-        type="radio"
-        name="prev"
-      >
-      <label for="cyol" :class="{ selected: zhqnSelected === false }">青年大学习</label>
-    </fieldset>
-    <fieldset class="section">
-      <legend>个人信息</legend>
-      <div class="pair">
-        <label for="name">姓名</label>
-        <input id="name" v-model="name" type="text" placeholder="请输入姓名">
-      </div>
-      <div class="pair">
-        <label for="id">学号</label>
-        <input id="id" v-model.number="id" type="text" placeholder="请输入学号">
-      </div>
-      <div id="info-action">
-        <button @click.prevent="submitInfo(name, id)">
-          提交信息
-        </button>
-        <button @click.prevent="reDraw">
-          刷新预览窗口
-        </button>
-      </div>
-    </fieldset>
-    <fieldset class="section">
-      <legend>导出设置</legend>
-      <div class="pair">
-        <label for="filename1">郑航青年截图文件名</label>
-        <input id="filename1" v-model="fileName1" type="text" :placeholder="`${id}${name}(1).jpg`">
-      </div>
-      <div class="pair">
-        <label for="filename2">青年大学习截图文件名</label>
-        <input id="filename2" v-model="fileName2" type="text" :placeholder="`${id}${name}(2).jpg`">
-      </div>
-      <button @click.prevent="download">
-        下载文件
-      </button>
-    </fieldset>
-  </fieldset>
-  <div id="top-ellipse" />
-  <div id="bottom-ellipse" />
-</template>
 
 <style lang="less">
 
